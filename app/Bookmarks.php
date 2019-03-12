59a674f66d4d813b5fc41bdfeaa140e07febe90b
Vladimir Anufriev <anufrievv.vvladimir@gmail.com>
2019-03-12 17:25:23 +0300

    Made bookmarks model, controller & migration. Made base auth. Added username field to user's migration. Added phpstorm meta info.

---
+++ app/Bookmarks.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Bookmarks
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookmarks newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookmarks newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Bookmarks query()
+ * @mixin \Eloquent
+ */
+class Bookmarks extends Model
+{
+    //
+}
